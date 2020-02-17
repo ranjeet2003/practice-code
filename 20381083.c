fc604ed063da6052673405a2ffa5446a0977de2a
chiku476 <61139330+chiku476@users.noreply.github.com>
2020-02-17 14:00:09 +0530

    Add files via upload

---
+++ 20381083.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main(void)
+{
+    int t,n,s,j,k;
+    scanf("%d",&t);
+    while(t--)
+    {
+        k=0;
+        scanf("%d",&n);
+        while(n--)
+        {
+            scanf("%d%d",&s,&j);
+            if(j>=s)
+            {
+            if((j-s)>5)
+            k++;
+            }
+            else
+            {
+                if(s>j)
+                {
+                    if((s-j)>5)
+                    k++;
+                }
+            }
+            
+        }
+        printf("%d\n",k);
+    }
+    return 0;
+}
