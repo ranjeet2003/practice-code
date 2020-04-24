26dca0afabd35337910b9398c6826051aca41eda
chiku476 <61139330+chiku476@users.noreply.github.com>
2020-04-24 10:42:36 +0530

    Add files via upload

---
+++ 32161667.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+int main() 
+{ int t;
+cin>>t;
+  while(t--){
+    string str;
+    cin>>str;
+     int k=0;
+    // Reverse str[beign..end] 
+   // reverse(str.begin(), str.end()); 
+  for(int i=str.length()-1;i>=0;i-- ){
+      //char  p=str[i];
+      if(str[i] != '0' || k ==1 ){
+          k=1;
+    cout << str[i]; }
+      
+  }
+    cout << endl;}
+    return 0; 
+} 
